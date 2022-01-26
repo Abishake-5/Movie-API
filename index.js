b63a8478c12be1323e744db49b5730b16d821774
Abishake-5 <ghanabiswa5@gmail.com>
2022-01-25 20:32:50 -0500

    Task 2.8
    added functionality such as able to add or remove users or their favorite movies that's stored in mongoDB

--- index.js
+++ index.js
@@ -1,127 +1,150 @@
 const morgan = require('morgan');
 const express = require('express')
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Models = require('./models');
+const Movie = Models.Movie;
+const User = Models.User;
 
- app.use(morgan('common'));
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(express.json());
+// middleware that allows me to post json data
+app.use(express.urlencoded({ extended: false}));
+// middleware that alows me to post keyvalue pairs of data
+app.use(morgan('common'));
+// logger that console logs date, time and method used
+app.use(bodyParser.json());
 
-let topMovies = [
-  {
-    title: 'The Lion King',
-    year:'2019',
-    genre:'',
-    directorName:''
-    
-  },
-  {
-    title: 'Sing',
-    year:'2019',
-    genre:'',
-    directorName:''
-  },
-  {
-    title: 'Sing 2 ',
-    year: '2021',
-    genre:'',
-    directorName:''
-  },
-    {
-    title: 'Encanto',
-    year: '2021',
-    genre:'',
-    directorName:''
-  },
-    {
-    title: 'Spider man no way home',
-    year: '2021',
-    genre:'',
-    directorName:''
-  },
-    {
-    title: 'Avengers End Game',
-    year: '2019',
-    genre:'',
-    directorName:''
-  },
-    {
-    title: 'naruto last movie',
-    year: '2014',
-    genre:'',
-    directorName:''
-  },
-    {
-    title: 'Demon Slayer: Mugen Train',
-    year: '2020',
-    genre:'',
-    directorName:''
-  },
-    {
-    title: 'Mr peabody and sherman',
-    year: '2014',
-    genre:'',
-    directorName:''
-  },
-    {
-    title: 'coco',
-    year: '2017',
-    genre:'',
-    directorName:''
-  }
-];
+app.use(express.static('public'));
+// This uses the use function from express to send file from whatever folder is defined in this case public. 
 
 // GET requests
 app.get('/', (req, res) => {
     res.send('MyFlix App');
-
 });
-
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+    Movie.find()
+    .then((movies) =>{
+        res.status(201).json(movies);
+    })
+    .catch((err) =>{
+        console.error(err);
+        res.status(500).send('Error:'+ err);
+    })
 });
-
-app.use(express.static('public'));
-app.use(function(req, res, err){
-  console.log(err)
-  res.status(500).send('Something Broke!')
+app.get('/movie/:title', (req, res) => {
+    Movie.findOne({'Title': req.params.title})
+    .then((movies)=>{
+        res.json(movies);
+    })
+    .catch((err)=>{
+        console.error(err);
+        res.status(500).send('Error' + err);
+    });
 });
-
-
-app.get('/movies/:title', (req, res) => {
-    res.send('Movie by title');
-});
-
 app.get('/catogries/:genre', (req, res)=> {
-  res.send("Movie Genre");
+  Movie.findOne({'Genre.Name': req.params.genre})
+    .then((movies)=>{
+        res.json(movies)
+    })
+    .catch((err)=>{
+        console.error(err);
+        res.status(500).send('Error' + err);
+    });
 });
-
 app.get('/directorName/:name', (req, res)=> {
-  res.send("Movie DirectorName");
+  Movie.findOne({'Director.Name': req.params.name})
+  .then((director) =>{
+    res.status(500).json(director)
+  })
+  .catch((err) =>{
+    console.error(err);
+  })
 });
-
-app.post('/users/:newUser', (req, res)=> {
- res.send("New User");
+app.post('/users', (req, res) => {
+  User.findOne({ Name: req.body.Name })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Name + ' Already exists');
+      } else {
+        User.create({
+            Name: req.body.Name,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            BirthDay: req.body.BirthDay
+          })
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
-
-app.put('/users/:username', (req, res)=> {
- res.send("Update User");
+app.put('/users/:name', (req, res)=> {
+  User.findOneAndUpdate({Name: req.params.name}, {$set:{
+    Name: req.body.Name,
+    Password: req.body.Password,
+    Email: req.body.Email,
+    BirthDay: req.body.BirthDay
+  }}, {new: true}/* The makes srue that the updated document is returned */ , (err, updatedUser) =>{
+    if (err){
+      res.status(500).send(err + 'Error')
+    }else{
+      res.status(200).json(updatedUser)
+    }
+  })
 });
-
-app.delete('/users/:username', (req, res)=> {
- res.send("Delete User");
+app.delete('/users/:name', (req, res)=> {
+ User.findOneAndRemove({Name: req.params.name})
+ .then((user)=>{
+   if (!user){
+     res.status(400).send(req.params.name + 'was not found');
+   }else{
+     res.status(200).send(req.params.name + " was deleted");
+   }
+ }).catch((err) =>{
+   res.status(500).send('Error'+ err);
+ })
 });
-
-app.post('/favorite/:movieName', (req, res)=> {
- res.send("Add Movies to Fav");
+app.post('/users/:name/:title', (req, res)=> {
+User.findOneAndUpdate({Name: req.params.name}, {$push:{FavMovies: req.params.title}}, {new: true},
+  (err, updateFavMovies)=>{
+    if (err){
+      res.status(500).send(err + ' Error');
+      console.error(err);
+    }else{
+      res.status(200).json(updateFavMovies);
+    }
+  })
 });
+app.delete('/users/:name/:title', (req, res)=> {
+ User.findOneAndUpdate({Name: req.params.name},{$pull: {FavMovies: req.params.title}},{new: true}, (err, updateFavMovies) =>{
+   if (err){
+     res.send(err)
+   }else{
+   res.status(200).json(updateFavMovies)
+   }
+ })
 
-app.get('/favorite', (req, res)=> {
- res.send("get a list of fav movies");
 });
+app.get('/users/:name', (req, res) =>{
+     User.findOne({'Name': req.params.name})
+    .then((user) => {
+        res.json(user.FavMovies);
+    })
+    .catch((err)=>{
+        console.error(err);
+        res.status(500).send('Error' + error);
+    });
 
-app.delete('/favorite/:movieName', (req, res)=> {
-   res.send("delete move from fav");
-
-});
+})
 // listen for requests
-app.listen(8080, () =>{
-  console.log('Your app is listening on port 8080.');
+app.listen(8080,()=>{
+  console.log('Listening to port 8080')
 });
---
+++ models.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+let movieSchema = new mongoose.Schema({
+    Title: { type: String, required:true},
+    Description:{ type: String, required: true},
+    Genre: {
+        Name: String,
+        Description: String
+    },
+    Director:{
+        Name: String,
+        Bio: String
+    },
+    Actors: [String],
+    ImagePath: String,
+    Featured: Boolean
+});
+let userSchema = new mongoose.Schema({
+    Name: {type: String, required: true},
+    Password: { type: String, required: true},
+    Email : { type: String, required: true},
+    BirthDay: Date,
+    FavMovies:[{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+})
+let Movie = mongoose.model('movies', movieSchema);
+let User = mongoose.model('user', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
+
